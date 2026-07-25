b10b1b049423145a9c09e71baecdac7e1d08db5e
schapman14663 <schapman14663@gmail.com>
2026-07-25 17:38:43 +0100

    tidying comments on rounding.rs

--- src/math_ops/rounding.rs
+++ src/math_ops/rounding.rs
@@ -1,13 +1,9 @@
 use crate::math_ops::div:weight_division;
 
 mod div;
-//rounding down is floor division
-//
-//ex floor(122.7/2.5) = 49
-//
-//you can get the number following the decimal (in the above example: 0.08) by doing the following:
-//(X % Y)/ Y
-//then to round down subtract this from the original division. 
+
+
+// Round the input weight down to the nearest multiple of the increment
 fn round_down(weight: f64, increment: f64) -> f64 {
     let (w, _) = weight_division(weight, increment);
     let rounded_weight = w * increment;
@@ -19,9 +15,7 @@ fn round_down(weight: f64, increment: f64) -> f64 {
     //remainder.
 }
 
-//rounding up is ceiling division
-//ex ceil(122.7/2.5) = 50
-//as above, but to always round up, you can add (1 - result) to the original division.
+// Round the input weight up to the nearest multiple of the increment
 fn round_up(weight: f64, increment: f64) -> f64 {
     let (w, r) = weight_division(weight, increment);
     if r > 0.0 {
@@ -33,12 +27,9 @@ fn round_up(weight: f64, increment: f64) -> f64 {
         println!("When rounding up the weight is: {:.2}", rounded_weight);
         rounded_weight
     }
-    //Not sure if this is actually written properly but, I guess we'll find out.
 }
 
-//smart division in this case means to the nearest integer regardless of direction.
-//the rules above apply, but for smart division we would run the rounding up check, then if (1 -
-//result) is above 0.5 apply the round up rule, otherwise apply the round down down rule.
+// Round the input weight to the nearest multiple of the increment regardless of direction
 fn smart_round(weight: f64, increment: f64) -> f64 {
     let (_,r) = weight_division(weight, increment);
     let remainder_ratio = r/increment;
@@ -48,8 +39,3 @@ fn smart_round(weight: f64, increment: f64) -> f64 {
         round_down(weight, increment)
     }
 }
-
-//standard barbell is 20kg or 45lbs and additional weight would be added to each side.
-//as such weight added to the bar is always halved. Ex 80kg added to the bar is 40kg each side. 
-//similarly a 2.5kg progression would actually be 1.25kg on each side of the bar.
-
