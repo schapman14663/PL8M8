248bf85b5e69467af72311b9e33af77b7f5ce958
schapman14663 <schapman14663@gmail.com>
2026-07-29 18:52:38 +0100

    trying to re-implement enums

--- src/weight_structs.rs
+++ src/weight_structs.rs
@@ -1,1 +1,19 @@
+pub mut struct plate_set {
+    weight: f64,
+    unit: unit_type,
+    rounding: rounding_type,
+    available_plates: HashMap<K, V>,
+}
 
+pub enum unit_type {
+    metric,
+    imperial,
+}
+
+pub enum rounding_type {
+    up,
+    down,
+    smart,
+}
+
+/* method to generate the available plates here */
